00bbfdf586076954d93020e24b93ae1e999da6c1
Sibyl-null <1286438810@qq.com>
2023-08-17 23:28:21 +0800

    [feat]: UIFramework - 队列调用 禁止隐藏/销毁非队头元素

--- UIFramework/Runtime/Scheduler/UIQueueScheduler.cs
+++ UIFramework/Runtime/Scheduler/UIQueueScheduler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using SFramework.Utility.Runtime;
 
 namespace SFramework.UIFramework.Runtime.Scheduler
 {
@@ -20,12 +21,24 @@ namespace SFramework.UIFramework.Runtime.Scheduler
 
         internal override void HideUI(UIEnumBaseType uiEnumType)
         {
+            if (_uiQueue.Count > 0 && _uiQueue.Peek() != uiEnumType)
+            {
+                DLog.Error("UI 队列调用: 不允许隐藏非队头元素");
+                return;
+            }
+            
             UIManager.Instance.HideUIInternal(uiEnumType);
             TryShowNextQueueUI(uiEnumType);
         }
 
         internal override void DestroyUI(UIEnumBaseType uiEnumType)
         {
+            if (_uiQueue.Count > 0 && _uiQueue.Peek() != uiEnumType)
+            {
+                DLog.Error("UI 队列调用: 不允许销毁非队头元素");
+                return;
+            }
+            
             UIManager.Instance.DestroyUIInternal(uiEnumType);
             TryShowNextQueueUI(uiEnumType);
         }
