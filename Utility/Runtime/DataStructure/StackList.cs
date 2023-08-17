33c0b669d44be0f50035213447600f8b836acf38
Sibyl-null <1286438810@qq.com>
2023-08-17 23:00:40 +0800

    [feat]: Utility - 新增StackList数据结构

---
+++ Utility/Runtime/DataStructure/StackList.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+namespace SFramework.Utility.Runtime.DataStructure
+{
+    public sealed class StackList<T>
+    {
+        private List<T> _list = new List<T>();
+
+        public int Count => _list.Count;
+        public int Capacity => _list.Capacity;
+
+        public StackList()
+        {
+        }
+
+        public StackList(int capacity)
+        {
+            _list.Capacity = capacity;
+        }
+
+        public void Push(T value)
+        {
+            _list.Add(value);
+        }
+
+        public T Pop()
+        {
+            T t = Peek();
+            _list.RemoveAt(Count - 1);
+            return t;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+                throw new Exception("StackList 为空，禁止Pop/Peek");
+
+            return _list[Count - 1];
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new Exception("StackList RemoveAt 下标越界");
+            
+            _list.RemoveAt(index);
+        }
+
+        public void Remove(T value)
+        {
+            _list.Remove(value);
+        }
+
+        public bool Container(T value)
+        {
+            return _list.Contains(value);
+        }
+    }
+}
