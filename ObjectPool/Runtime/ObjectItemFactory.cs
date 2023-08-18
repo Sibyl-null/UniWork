c4d3c5c9dc6d0fa072a3b7b47dd59e0f98eaddef
Sibyl-null <1286438810@qq.com>
2023-08-19 01:26:28 +0800

    [feat]: ObjectPool - 简易对象池实现

---
+++ ObjectPool/Runtime/ObjectItemFactory.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SFramework.ObjectPool.Runtime
+{
+    internal class ObjectItemFactory
+    {
+        private Transform _factoryTrans;
+        private readonly Queue<GameObject> _queue;
+        private GameObject _objectPrefab;
+        private string _loadPath;
+
+        public ObjectItemFactory(string loadPath, int initCount, Transform factoryTrans)
+        {
+            _factoryTrans = factoryTrans;
+            _factoryTrans.name = "[Factory] " + loadPath;
+            _loadPath = loadPath;
+            _queue = new Queue<GameObject>(initCount);
+            _objectPrefab = ObjectPool.LoadAssetFunc.Invoke(loadPath);
+            PreLoad(initCount);
+        }
+
+        public void PreLoad(int count)
+        {
+            if (count <= _queue.Count)
+                return;
+
+            int needCount = count - _queue.Count;
+            for (int i = 0; i < needCount; ++i)
+            {
+                GameObject obj = GameObject.Instantiate(_objectPrefab, _factoryTrans);
+                obj.SetActive(false);
+                _queue.Enqueue(obj);
+            }
+        }
+
+        public GameObject Get()
+        {
+            if (_queue.Count > 0)
+            {
+                GameObject obj = _queue.Dequeue();
+                obj.SetActive(true);
+                return obj;
+            }
+
+            return GameObject.Instantiate(_objectPrefab, _factoryTrans);
+        }
+
+        public void Recycle(GameObject obj)
+        {
+            obj.SetActive(false);
+            obj.transform.SetParent(_factoryTrans);
+            _queue.Enqueue(obj);
+        }
+
+        public void Destroy()
+        {
+            _queue.Clear();
+            GameObject.Destroy(_factoryTrans.gameObject);
+            ObjectPool.ReleaseAssetAction?.Invoke(_loadPath);
+            _objectPrefab = null;
+        }
+    }
+}
---
+++ ObjectPool/Runtime/ObjectPool.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using SFramework.Utility.Runtime;
+using UnityEngine;
+
+namespace SFramework.ObjectPool.Runtime
+{
+    public static class ObjectPool
+    {
+        internal static Func<string, GameObject> LoadAssetFunc;
+        internal static Action<string> ReleaseAssetAction;
+        internal static int DefaultInitCount;
+
+        private static readonly Dictionary<string, ObjectItemFactory> _factories =
+            new Dictionary<string, ObjectItemFactory>();
+
+        private static Transform _objectPoolTrans;
+        private static bool _hasInit = false;
+
+        public static void Init(Func<string, GameObject> loadAssetFunc, Action<string> releaseAssetAction,
+            int defaultInitCount = 20)
+        {
+            LoadAssetFunc = loadAssetFunc;
+            DefaultInitCount = defaultInitCount;
+            
+            GameObject poolObj = new GameObject("ObjectPool");
+            GameObject.DontDestroyOnLoad(poolObj);
+            _objectPoolTrans = poolObj.transform;
+            
+            _hasInit = true;
+        }
+
+        private static void MakeSureInit()
+        {
+            if (_hasInit == false)
+                throw new Exception("ObjectPool 未初始化");
+            if (LoadAssetFunc == null)
+                throw new Exception("ObjectPool LoadAssetFunc为null");
+        }
+
+        public static GameObject Get(string loadPath)
+        {
+            MakeSureInit();
+
+            if (_factories.ContainsKey(loadPath) == false)
+            {
+                GameObject factoryObj = new GameObject();
+                factoryObj.transform.SetParent(_objectPoolTrans);
+                _factories.Add(loadPath, new ObjectItemFactory(loadPath, DefaultInitCount, factoryObj.transform));
+            }
+
+            return _factories[loadPath].Get();
+        }
+
+        public static void Recycle(string loadPath, GameObject obj)
+        {
+            MakeSureInit();
+
+            if (_factories.TryGetValue(loadPath, out ObjectItemFactory factory))
+                factory.Recycle(obj);
+            else
+                DLog.Error("不存在该对象池: " + loadPath);
+        }
+
+        public static void PreLoad(string loadPath, int initCount)
+        {
+            MakeSureInit();
+            
+            if (_factories.TryGetValue(loadPath, out ObjectItemFactory factory))
+            {
+                factory.PreLoad(initCount);
+            }
+            else
+            {
+                GameObject factoryObj = new GameObject();
+                factoryObj.transform.SetParent(_objectPoolTrans);
+                _factories.Add(loadPath, new ObjectItemFactory(loadPath, initCount, factoryObj.transform));
+            }
+        }
+
+        public static void DestroyPool(string loadPath)
+        {
+            MakeSureInit();
+
+            if (_factories.TryGetValue(loadPath, out ObjectItemFactory factory))
+            {
+                factory.Destroy();
+                _factories.Remove(loadPath);
+            }
+            else
+                DLog.Error("不存在该对象池: " + loadPath);
+        }
+    }
+}
