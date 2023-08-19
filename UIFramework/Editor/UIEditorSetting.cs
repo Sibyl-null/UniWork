479114c085ee57e76122c860394e64e26e2598e1
Sibyl-null <1286438810@qq.com>
2023-08-19 21:22:30 +0800

    [feat]: UIFramework - EditorSetting添加AutoBind默认值

--- UIFramework/Editor/UIEditorSetting.cs
+++ UIFramework/Editor/UIEditorSetting.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SFramework.UIFramework.Editor
 {
@@ -12,11 +14,31 @@ namespace SFramework.UIFramework.Editor
         {
             public string prefix;
             public string componentName;
+
+            public AutoBindData(string prefix, string componentName)
+            {
+                this.prefix = prefix;
+                this.componentName = componentName;
+            }
         }
 
         [FolderPath]
         public string codeFileSavePath = "Scripts/AutoGen/UI";
         public string codeNamespace = "UI";
-        public List<AutoBindData> autoBindComponents = new List<AutoBindData>();
+
+        public List<AutoBindData> autoBindComponents = new List<AutoBindData>()
+        {
+            new AutoBindData("txt_", nameof(Text)),
+            new AutoBindData("txt_", nameof(TextMeshProUGUI)),
+            new AutoBindData("btn_", nameof(Button)),
+            new AutoBindData("img_", nameof(Image)),
+            new AutoBindData("rawImg_", nameof(RawImage)),
+            new AutoBindData("trans_", nameof(RectTransform)),
+            new AutoBindData("go_", nameof(GameObject)),
+            new AutoBindData("sv_", nameof(ScrollRect)),
+            new AutoBindData("input_", nameof(InputField)),
+            new AutoBindData("cg_", nameof(CanvasGroup)),
+            new AutoBindData("dp_", nameof(Dropdown))
+        };
     }
 }
