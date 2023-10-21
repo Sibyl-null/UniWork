dd640186b44db0462fcd6e2d33f4016a16a4e208
Sibyl-null <1286438810@qq.com>
2023-10-21 21:50:17 +0800

    [feat]: BehaviourTree - 根节点和BehaviourTree实现

---
+++ BehaviourTree/Runtime/BehaviourTree.cs
@@ -0,0 +1,49 @@
+﻿using SFramework.BehaviourTree.Runtime.Node;
+using SFramework.Utility.Runtime;
+
+namespace SFramework.BehaviourTree.Runtime
+{
+    public class BehaviourTree
+    {
+        public RootNode rootNode;
+
+        public static BehaviourTree Create()
+        {
+            return new BehaviourTree();
+        }
+
+        public void Start()
+        {
+            if (rootNode == null)
+            {
+                DLog.Error("[BehaviourTree] RootNode is Null");
+                return;
+            }
+            
+            if (rootNode.curState == BaseNode.State.Running)
+                return;
+            
+            rootNode.Start();
+        }
+
+        public void Cancel()
+        {
+            if (rootNode == null)
+            {
+                DLog.Error("[BehaviourTree] RootNode is Null");
+                return;
+            }
+            
+            if (rootNode.curState != BaseNode.State.Running)
+                return;
+            
+            rootNode.Cancel();
+        }
+
+        public void ReStart()
+        {
+            Cancel();
+            rootNode.Start();
+        }
+    }
+}
---
+++ BehaviourTree/Runtime/Node/RootNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SFramework.BehaviourTree.Runtime.Node.Decorate;
+
+namespace SFramework.BehaviourTree.Runtime.Node
+{
+    /// <summary>
+    /// 根节点
+    /// </summary>
+    public class RootNode : BaseDecorateNode
+    {
+        public event Action<bool> onFinishEvent;
+
+        protected override void OnChildFinished(BaseNode child, bool success)
+        {
+            Finish(success);
+            onFinishEvent?.Invoke(success);
+        }
+    }
+}
