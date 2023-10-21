72d69ef9e7116d13b619efde83d149f9741de0be
Sibyl-null <1286438810@qq.com>
2023-10-21 20:02:05 +0800

    [feat]: BehaviourTree - 复合节点实现

---
+++ BehaviourTree/Runtime/Node/BaseNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SFramework.BehaviourTree.Runtime.Node
+{
+    public abstract class BaseNode
+    {
+        public enum State
+        {
+            Free,       // 空闲
+            Running,    // 运行中
+            Success,    // 成功
+            Failed      // 失败
+        }
+
+        public State curState = State.Free;
+        public BaseNode parentNode;
+
+        protected abstract void AddChild(BaseNode child);
+        protected abstract void RemoveChild(BaseNode child);
+        protected abstract void ClearChildren();
+        protected abstract void ForeachChildren(Action<BaseNode> action);
+
+        public void Start()
+        {
+            if (curState == State.Running)
+                return;
+
+            curState = State.Running;
+            OnStart();
+        }
+
+        public void Cancel()
+        {
+            if (curState != State.Running)
+                return;
+
+            curState = State.Free;
+            OnCancel();
+        }
+
+        public void Finish(bool success)
+        {
+            curState = success ? State.Success : State.Failed;
+            // 通知父节点自身运行结果
+            parentNode?.OnChildFinished(this, success);
+        }
+
+        protected abstract void OnStart();
+        protected abstract void OnCancel();
+        protected abstract void OnChildFinished(BaseNode child, bool success);
+
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+}
---
+++ BehaviourTree/Runtime/Node/Composite/BaseCompositeNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SFramework.BehaviourTree.Runtime.Node.Composite
+{
+    /// <summary>
+    /// 装饰节点基类
+    /// </summary>
+    public abstract class BaseCompositeNode : BaseNode
+    {
+        protected readonly List<BaseNode> _children = new List<BaseNode>();
+
+        protected override void AddChild(BaseNode child)
+        {
+            if (child == null)
+                return;
+
+            child.parentNode = this;
+            _children.Add(child);
+        }
+
+        protected override void RemoveChild(BaseNode child)
+        {
+            if (child == null)
+                return;
+
+            child.parentNode = null;
+            _children.Remove(child);
+        }
+
+        protected override void ClearChildren()
+        {
+            for (int i = _children.Count - 1; i >= 0; --i)
+                RemoveChild(_children[i]);
+        }
+
+        protected override void ForeachChildren(Action<BaseNode> action)
+        {
+            if (action == null)
+                return;
+            
+            foreach (BaseNode child in _children)
+                action.Invoke(child);
+        }
+    }
+}
