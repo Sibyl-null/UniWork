1a4a5dfeb5ba6a181c660f67c568b417c7368bac
Sibyl-null <1286438810@qq.com>
2023-10-22 13:45:13 +0800

    [feat]: Utility - 添加RectTransform全屏设置扩展方法

--- Utility/Runtime/ExtendMethods.cs
+++ Utility/Runtime/ExtendMethods.cs
@@ -5,6 +5,12 @@ namespace SFramework.Utility.Runtime
 {
     public static class ExtendMethods
     {
+        public static void DLogArrayInfos(this Array arr)
+        {
+            foreach (object o in arr)
+                DLog.Info(o.ToString());
+        }
+        
         public static T GetOrAddComponent<T>(this GameObject obj) where T : Component
         {
             T t = obj.GetComponent<T>();
@@ -15,10 +21,17 @@ namespace SFramework.Utility.Runtime
             return t;
         }
 
-        public static void DLogArrayInfos(this Array arr)
+        /// <summary>
+        /// UI 控件布局设置为全屏
+        /// </summary>
+        public static void Overspread(this RectTransform rectTrans)
         {
-            foreach (object o in arr)
-                DLog.Info(o.ToString());
+            rectTrans.pivot = 0.5f * Vector2.one;
+            rectTrans.localScale = Vector3.one;
+            rectTrans.offsetMin = Vector2.zero;
+            rectTrans.offsetMax = Vector2.zero;
+            rectTrans.anchorMin = Vector2.zero;
+            rectTrans.anchorMax = Vector2.one;
         }
     }
 }
