725c5c3ebe585316c4ae0a3f5f69dd8562869d13
Sibyl-null <1286438810@qq.com>
2023-12-01 23:44:25 +0800

    [feat]: UniBundle - CollectAssetsTask实现

---
+++ UniBundle/Editor/BuildContexts/AssetInfoContainer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace UniWork.UniBundle.Editor.BuildContexts
+{
+    internal class AssetInfo
+    {
+        public string Name;
+        public string NameNoExt;
+        public string AssetPath;
+        public int RefCount;
+        public string BundleName;
+
+        internal AssetInfo(string assetPath)
+        {
+            Name = Path.GetFileName(assetPath);
+            NameNoExt = Path.GetFileNameWithoutExtension(assetPath);
+            AssetPath = assetPath;
+            RefCount = 0;
+            BundleName = "";
+        }
+    }
+    
+    internal class AssetInfoContainer : IContextData
+    {
+        // AssetPath -> AssetInfo
+        internal readonly Dictionary<string, AssetInfo> Infos = new Dictionary<string, AssetInfo>();
+
+        internal void RecordAsset(string assetPath)
+        {
+            if (Infos.TryGetValue(assetPath, out AssetInfo info))
+            {
+                ++info.RefCount;
+            }
+            else
+            {
+                info = new AssetInfo(assetPath);
+                Infos.Add(assetPath, info);
+            }
+        }
+    }
+}
