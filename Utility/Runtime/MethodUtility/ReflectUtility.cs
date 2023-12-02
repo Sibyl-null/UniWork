9409d46cb07d85a6d167679afaa829f513f8d0c5
Sibyl-null <1286438810@qq.com>
2023-12-02 21:59:36 +0800

    [feat]: Utility - 文件位置修改，反射帮助方法实现

---
+++ Utility/Runtime/MethodUtility/ReflectUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace UniWork.Utility.Runtime.MethodUtility
+{
+    public static class ReflectUtility
+    {
+        public static object InvokeNonPublicStaticMethod(Type type, string method, params object[] parameters)
+        {
+            MethodInfo methodInfo = type.GetMethod(method, BindingFlags.NonPublic | BindingFlags.Static);
+            if (methodInfo == null)
+            {
+                DLog.Error($"{type.FullName} not found method : {method}");
+                return null;
+            }
+
+            return methodInfo.Invoke(null, parameters);
+        }
+    }
+}
