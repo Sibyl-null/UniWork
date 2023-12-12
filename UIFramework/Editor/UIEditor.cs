e4551fd612c354bba759a79493cb0703e4d99c9d
yangboyuan <yangboyuan@dailyinnovation.biz>
2023-12-12 18:36:14 +0800

    [feat]: UIFramework - 代码生成逻辑拆分

--- UIFramework/Editor/UIEditor.cs
+++ UIFramework/Editor/UIEditor.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using UnityEditor;
-using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -14,14 +9,14 @@ using UniWork.Utility.Runtime;
 
 namespace UniWork.UIFramework.Editor
 {
-    public static class UIEditor
+    public static partial class UIEditor
     {
         // ---------------------------------------------------------------
         // 初始化资源
         // ---------------------------------------------------------------
 
         private const string UIRootDefaultSavePath = "Assets/Resources/UIRoot.prefab";
-        private const string UIEditorSettingDefaultSavePath = "Assets/Editor/Config/UIEditorSetting.asset";
+        public const string UIEditorSettingDefaultSavePath = "Assets/Editor/Config/UIEditorSetting.asset";
         private const string AutoBindTag = "AutoField";
 
         [MenuItem("UniWork/UIFramework/创建全部", false, 1)]
@@ -159,7 +154,6 @@ namespace UniWork.UIFramework.Editor
         // ---------------------------------------------------------------
 
         private const int MenuItemPriority = 100;
-        private const string AutoGenScriptNameKey = "AutoGenScriptName";
         
         [MenuItem("Assets/创建UITemplate模板", false, MenuItemPriority)]
         public static void CreateUITemplate()
@@ -203,180 +197,6 @@ namespace UniWork.UIFramework.Editor
                 selectedPath = AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]);
             
             return selectedPath;
-        }
-        
-        [MenuItem("Assets/自动生成UIView代码", false, MenuItemPriority)]
-        public static void GenerateUIViewCode()
-        {
-            // 1. 非法情况判断
-            string selectedPath = GetSelectedPath();
-            if (selectedPath == "" || File.Exists(selectedPath) == false)
-                throw new Exception("[自动生成UIView代码]: 请选择一个Prefab");
-
-            GameObject selectedObject = AssetDatabase.LoadAssetAtPath<GameObject>(selectedPath);
-            if (selectedObject == null)
-                throw new Exception("[自动生成UIView代码]: 请选择一个Prefab");
-
-            UIEditorSetting editorSetting =
-                AssetDatabase.LoadAssetAtPath<UIEditorSetting>(UIEditorSettingDefaultSavePath);
-            if (editorSetting == null)
-                throw new Exception("[自动生成UIView代码]: UIEditorSetting 加载失败");
-            if (string.IsNullOrEmpty(editorSetting.codeFileSavePath))
-                throw new Exception("[自动生成UIView代码]: 代码保存路径未设定");
-
-            // 2. 数据收集
-            HashSet<string> namespaceSet = new HashSet<string> { "UniWork.UIFramework.Runtime" };
-            Dictionary<string, string> goNamePathMap = new Dictionary<string, string>();
-            List<(string typeName, string fieldName, string goName)> fieldList =
-                new List<(string typeName, string fieldName, string goName)>();
-
-            List<GameObject> childList = new List<GameObject>();
-            GetAllChildGameObjects(selectedObject.transform, ref childList);
-
-            var gameObjectBindData =
-                editorSetting.autoBindComponents.Where(bindData => bindData.componentName == nameof(GameObject))
-                    .ToArray();
-            bool shouldBindGameObject = gameObjectBindData.Length > 0;
-
-            foreach (GameObject childObj in childList)
-            {
-                if (childObj.CompareTag(AutoBindTag) == false)
-                    continue;
-
-                if (goNamePathMap.ContainsKey(childObj.name))
-                    throw new Exception("[自动生成UIView代码]: 存在对象重名 " + childObj.name);
-                goNamePathMap.Add(childObj.name, GetChildFindPath(childObj.transform));
-
-                Component[] components = childObj.GetComponents<Component>();
-                foreach (Component component in components)
-                {
-                    foreach (var autoBindData in editorSetting.autoBindComponents
-                                 .Where(bindData => bindData.componentName == component.GetType().Name))
-                    {
-                        namespaceSet.Add(component.GetType().Namespace);
-                        fieldList.Add((autoBindData.componentName, autoBindData.prefix + childObj.name, childObj.name));
-                        break;
-                    }
-                }
-
-                if (shouldBindGameObject)
-                {
-                    namespaceSet.Add(typeof(GameObject).Namespace);
-                    fieldList.Add((nameof(GameObject), gameObjectBindData[0].prefix + childObj.name, childObj.name));
-                }
-            }
-            
-            // 3. 代码生成
-            StringBuilder sb = new StringBuilder();
-            
-            // generate code info
-            sb.AppendLine($"// Auto generate at {DateTime.Now.Date}");
-            sb.AppendLine("// please do not modify this file");
-            sb.AppendLine();
-            
-            // namespace
-            foreach (string space in namespaceSet)
-                sb.AppendLine($"using {space};");
-            sb.AppendLine();
-
-            if (string.IsNullOrEmpty(editorSetting.codeNamespace) == false)
-            {
-                sb.AppendLine($"namespace {editorSetting.codeNamespace}");
-                sb.AppendLine("{");
-            }
-
-            // class name
-            sb.AppendLine($"\tpublic partial class {selectedObject.name}View : UIBaseView");
-            sb.AppendLine("\t{");
-            
-            // field
-            foreach (var tuple in fieldList)
-                sb.AppendLine($"\t\tpublic {tuple.typeName} {tuple.fieldName};");
-
-            // bind method
-            sb.AppendLine();
-            sb.AppendLine("\t\t// only editor use");
-            sb.AppendLine("\t\tprivate void BindComponent()");
-            sb.AppendLine("\t\t{");
-            
-            foreach (var p in goNamePathMap)
-                sb.AppendLine($"\t\t\tvar {p.Key} = transform.Find(\"{p.Value}\");");
-            sb.AppendLine();
-            foreach (var tuple in fieldList)
-            {
-                sb.AppendLine(tuple.typeName != nameof(GameObject)
-                    ? $"\t\t\t{tuple.fieldName} = {tuple.goName}.GetComponent<{tuple.typeName}>();"
-                    : $"\t\t\t{tuple.fieldName} = {tuple.goName}.gameObject;");
-            }
-
-            sb.AppendLine("\t\t}");
-
-            // end
-            sb.AppendLine("\t}");
-            if (string.IsNullOrEmpty(editorSetting.codeNamespace) == false)
-                sb.AppendLine("}");
-
-            string savePath = Path.Combine(editorSetting.codeFileSavePath, $"{selectedObject.name}View.cs");
-            if (Directory.Exists(editorSetting.codeFileSavePath) == false)
-                Directory.CreateDirectory(editorSetting.codeFileSavePath);
-            
-            File.WriteAllText(savePath, sb.ToString());
-            
-            AssetDatabase.Refresh();
-            DLog.Info("[自动生成UIView代码]: 成功! " + savePath);
-            
-            // 4. 脚本挂载并绑定
-            EditorPrefs.SetString(AutoGenScriptNameKey, $"{selectedObject.name}View");
-        }
-
-        [DidReloadScripts]
-        private static void AutoAddComponent()
-        {
-            GameObject prefab = Selection.activeObject as GameObject;
-            string scriptName = EditorPrefs.GetString(AutoGenScriptNameKey, "");
-
-            if (string.IsNullOrEmpty(scriptName) || prefab == null ||
-                prefab.name != scriptName.Substring(0, scriptName.Length - 4))
-                return;
-            
-            UIEditorSetting editorSetting =
-                AssetDatabase.LoadAssetAtPath<UIEditorSetting>(UIEditorSettingDefaultSavePath);
-            string scriptPath = Path.Combine(editorSetting.codeFileSavePath, scriptName + ".cs");
-
-            Type scriptType = AssetDatabase.LoadAssetAtPath<MonoScript>(scriptPath).GetClass();
-            if (prefab.GetComponent(scriptType) == null)
-                prefab.AddComponent(scriptType);
-
-            Component component = prefab.GetComponent(scriptType);
-            MethodInfo methodInfo =
-                scriptType.GetMethod("BindComponent", BindingFlags.Instance | BindingFlags.NonPublic);
-            methodInfo.Invoke(component, new object[] { });
-
-            PrefabUtility.SavePrefabAsset(prefab);
-            AssetDatabase.Refresh();
-
-            EditorPrefs.SetString(AutoGenScriptNameKey, "");
-            DLog.Info($"脚本添加成功: {scriptName}.cs");
-        }
-
-        private static void GetAllChildGameObjects(Transform parent, ref List<GameObject> result)
-        {
-            result.Add(parent.gameObject);
-            foreach (Transform childTrans in parent)
-                GetAllChildGameObjects(childTrans, ref result);
-        }
-
-        private static string GetChildFindPath(Transform child)
-        {
-            string findPath = child.name;
-            
-            while (child.parent != null && child.parent.parent != null)
-            {
-                child = child.parent;
-                findPath = child.name + "/" + findPath;
-            }
-
-            return findPath;
         }
     }
 }
---
+++ UIFramework/Editor/UIEditor_CodeGenerator.cs
@@ -0,0 +1,217 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UniWork.UIFramework.Runtime;
+using UniWork.Utility.Runtime;
+
+namespace UniWork.UIFramework.Editor
+{
+    internal class CodeGenerateData
+    {
+        public string YourNamespace;
+        public string ClassName;
+        public string[] Namespaces;
+        public Dictionary<string, string> GoNamePathMap;
+        public List<(string typeName, string fieldName, string goName)> FieldList;
+    }
+    
+    public static partial class UIEditor
+    {
+        private const string AutoGenScriptNameKey = "AutoGenScriptName";
+        
+        [MenuItem("Assets/自动生成UIView代码", false, MenuItemPriority)]
+        public static void GenerateUIViewCode()
+        {
+            // 1. 非法情况判断
+            string selectedPath = GetSelectedPath();
+            if (selectedPath == "" || File.Exists(selectedPath) == false)
+                throw new Exception("[自动生成UIView代码]: 请选择一个Prefab");
+
+            GameObject selectedObject = AssetDatabase.LoadAssetAtPath<GameObject>(selectedPath);
+            if (selectedObject == null)
+                throw new Exception("[自动生成UIView代码]: 请选择一个Prefab");
+
+            UIEditorSetting editorSetting =
+                AssetDatabase.LoadAssetAtPath<UIEditorSetting>(UIEditorSettingDefaultSavePath);
+            if (editorSetting == null)
+                throw new Exception("[自动生成UIView代码]: UIEditorSetting 加载失败");
+            if (string.IsNullOrEmpty(editorSetting.codeFileSavePath))
+                throw new Exception("[自动生成UIView代码]: 代码保存路径未设定");
+
+            // 2. 数据收集
+            HashSet<string> namespaceSet = new HashSet<string> { typeof(UIBaseView).Namespace };
+            Dictionary<string, string> goNamePathMap = new Dictionary<string, string>();
+            List<(string typeName, string fieldName, string goName)> fieldList =
+                new List<(string typeName, string fieldName, string goName)>();
+
+            List<GameObject> childList = new List<GameObject>();
+            GetAllChildGameObjects(selectedObject.transform, ref childList);
+
+            var gameObjectBindData =
+                editorSetting.autoBindComponents.Where(bindData => bindData.componentName == nameof(GameObject))
+                    .ToArray();
+            bool shouldBindGameObject = gameObjectBindData.Length > 0;
+
+            foreach (GameObject childObj in childList)
+            {
+                if (childObj.CompareTag(AutoBindTag) == false)
+                    continue;
+
+                if (goNamePathMap.ContainsKey(childObj.name))
+                    throw new Exception("[自动生成UIView代码]: 存在对象重名 " + childObj.name);
+                goNamePathMap.Add(childObj.name, GetChildFindPath(childObj.transform));
+
+                Component[] components = childObj.GetComponents<Component>();
+                foreach (Component component in components)
+                {
+                    foreach (var autoBindData in editorSetting.autoBindComponents
+                                 .Where(bindData => bindData.componentName == component.GetType().Name))
+                    {
+                        namespaceSet.Add(component.GetType().Namespace);
+                        fieldList.Add((autoBindData.componentName, autoBindData.prefix + childObj.name, childObj.name));
+                        break;
+                    }
+                }
+
+                if (shouldBindGameObject)
+                {
+                    namespaceSet.Add(typeof(GameObject).Namespace);
+                    fieldList.Add((nameof(GameObject), gameObjectBindData[0].prefix + childObj.name, childObj.name));
+                }
+            }
+
+            CodeGenerateData data = new CodeGenerateData
+            {
+                YourNamespace = editorSetting.codeNamespace,
+                ClassName = selectedObject.name,
+                Namespaces = namespaceSet.ToArray(),
+                GoNamePathMap = goNamePathMap,
+                FieldList = fieldList
+            };
+
+            string code = GenerateCode(data);
+
+            string savePath = Path.Combine(editorSetting.codeFileSavePath, $"{selectedObject.name}View.cs");
+            if (Directory.Exists(editorSetting.codeFileSavePath) == false)
+                Directory.CreateDirectory(editorSetting.codeFileSavePath);
+            
+            File.WriteAllText(savePath, code);
+            
+            AssetDatabase.Refresh();
+            DLog.Info("[自动生成UIView代码]: 成功! " + savePath);
+            
+            // 4. 脚本挂载并绑定
+            EditorPrefs.SetString(AutoGenScriptNameKey, $"{selectedObject.name}View");
+        }
+
+        private static string GenerateCode(CodeGenerateData data)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            // generate code info
+            sb.AppendLine($"// Auto generate at {DateTime.Now.Date}");
+            sb.AppendLine("// please do not modify this file");
+            sb.AppendLine();
+            
+            // namespace
+            foreach (string space in data.Namespaces)
+                sb.AppendLine($"using {space};");
+            sb.AppendLine();
+
+            if (string.IsNullOrEmpty(data.YourNamespace) == false)
+            {
+                sb.AppendLine($"namespace {data.YourNamespace}");
+                sb.AppendLine("{");
+            }
+
+            // class name
+            sb.AppendLine($"\tpublic partial class {data.ClassName}View : UIBaseView");
+            sb.AppendLine("\t{");
+            
+            // field
+            foreach (var tuple in data.FieldList)
+                sb.AppendLine($"\t\tpublic {tuple.typeName} {tuple.fieldName};");
+
+            // bind method
+            sb.AppendLine();
+            sb.AppendLine("\t\t// only editor use");
+            sb.AppendLine("\t\tprivate void BindComponent()");
+            sb.AppendLine("\t\t{");
+            
+            foreach (var p in data.GoNamePathMap)
+                sb.AppendLine($"\t\t\tvar {p.Key} = transform.Find(\"{p.Value}\");");
+            sb.AppendLine();
+            foreach (var tuple in data.FieldList)
+            {
+                sb.AppendLine(tuple.typeName != nameof(GameObject)
+                    ? $"\t\t\t{tuple.fieldName} = {tuple.goName}.GetComponent<{tuple.typeName}>();"
+                    : $"\t\t\t{tuple.fieldName} = {tuple.goName}.gameObject;");
+            }
+
+            sb.AppendLine("\t\t}");
+
+            // end
+            sb.AppendLine("\t}");
+            if (string.IsNullOrEmpty(data.YourNamespace) == false)
+                sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        [DidReloadScripts]
+        private static void AutoAddComponent()
+        {
+            GameObject prefab = Selection.activeObject as GameObject;
+            string scriptName = EditorPrefs.GetString(AutoGenScriptNameKey, "");
+
+            if (string.IsNullOrEmpty(scriptName) || prefab == null ||
+                prefab.name != scriptName.Substring(0, scriptName.Length - 4))
+                return;
+            
+            UIEditorSetting editorSetting =
+                AssetDatabase.LoadAssetAtPath<UIEditorSetting>(UIEditorSettingDefaultSavePath);
+            string scriptPath = Path.Combine(editorSetting.codeFileSavePath, scriptName + ".cs");
+
+            Type scriptType = AssetDatabase.LoadAssetAtPath<MonoScript>(scriptPath).GetClass();
+            if (prefab.GetComponent(scriptType) == null)
+                prefab.AddComponent(scriptType);
+
+            Component component = prefab.GetComponent(scriptType);
+            MethodInfo methodInfo =
+                scriptType.GetMethod("BindComponent", BindingFlags.Instance | BindingFlags.NonPublic);
+            methodInfo.Invoke(component, new object[] { });
+
+            PrefabUtility.SavePrefabAsset(prefab);
+            AssetDatabase.Refresh();
+
+            EditorPrefs.SetString(AutoGenScriptNameKey, "");
+            DLog.Info($"脚本添加成功: {scriptName}.cs");
+        }
+
+        private static void GetAllChildGameObjects(Transform parent, ref List<GameObject> result)
+        {
+            result.Add(parent.gameObject);
+            foreach (Transform childTrans in parent)
+                GetAllChildGameObjects(childTrans, ref result);
+        }
+
+        private static string GetChildFindPath(Transform child)
+        {
+            string findPath = child.name;
+            
+            while (child.parent != null && child.parent.parent != null)
+            {
+                child = child.parent;
+                findPath = child.name + "/" + findPath;
+            }
+
+            return findPath;
+        }
+    }
+}
