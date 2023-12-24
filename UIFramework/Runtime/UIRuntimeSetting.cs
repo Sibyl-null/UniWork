69f71916c1955775bd5915ab543a51babc448336
Sibyl-null <1286438810@qq.com>
2023-12-24 21:32:11 +0800

    [feat]: UIFramework - RuntimeSetting Inspector 美化

--- UIFramework/Runtime/UIRuntimeSetting.cs
+++ UIFramework/Runtime/UIRuntimeSetting.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace UniWork.UIFramework.Runtime
@@ -14,6 +15,8 @@ namespace UniWork.UIFramework.Runtime
 
         public int layerOrderOnceRaise = 10;
         public GameObject rootPrefab;
+        
+        [Title("UI 层级配置 (间距尽量大，填满 0 ~ 23e)"), TableList(ShowIndexLabels = true)]
         public List<ShowLayer> showLayers = new List<ShowLayer>();
     }
 }
