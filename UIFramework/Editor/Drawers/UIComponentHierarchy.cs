1fdc54dcdb3f05c2a329354115cb183725fa2790
Sibyl-null <1286438810@qq.com>
2024-02-16 21:26:47 +0800

    [feat]: UIFramework - Hierarchy 提示

---
+++ UIFramework/Editor/Drawers/UIComponentHierarchy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UniWork.UIFramework.Runtime;
+
+namespace UniWork.UIFramework.Editor.Drawers
+{
+    public static class UIComponentHierarchy
+    {
+        [InitializeOnLoadMethod]
+        private static void StartInitializeOnLoadMethod()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
+        }
+
+        private static void OnHierarchyGUI(int instanceID, Rect selectionRect)
+        {
+            GameObject go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+            if (go == null)
+                return;
+
+            UIComponentCollector collector = go.GetComponentInParent<UIComponentCollector>();
+            if (collector == null || collector.gameObject == go)
+                return;
+
+            List<Object> components = go.GetComponents<Component>().Cast<Object>().ToList();
+            components.Add(go);
+
+            if (collector.ComponentInfos.Count(x => components.Contains(x.component)) > 0)
+            {
+                Rect r = new Rect(selectionRect)
+                {
+                    x = 34,
+                    width = 80
+                };
+                
+                GUIStyle style = new GUIStyle();
+                style.normal.textColor = Color.yellow;
+                
+                GUI.Label(r, "★", style);
+            }
+        }
+    }
+}
